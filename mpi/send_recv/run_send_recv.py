a3fb3945776315b17c6ad9428c1178112fbc060f
macx <cxma@protonmail.ch>
2019-12-16 18:42:20 +0100

    Send & receive data similar to that of Flight benchmark

---
+++ mpi/send_recv/run_send_recv.py
@@ -0,0 +1,12 @@
+import subprocess
+KILO = 1000
+
+for num_of_records in range(100*KILO, 4000*KILO, 200*KILO):
+    for _ in range(10):
+        subprocess.call(["mpirun",
+                         "--map-by", "node",
+                         "--host", "tcn538,tcn658",
+                         "--np", str(2),
+                         "./send_recv",
+                         str(num_of_records)
+                         ])
--- mpi/send_recv/send_recv.c
+++ mpi/send_recv/send_recv.c
@@ -5,8 +5,22 @@ Source code link: https://github.com/wesleykendall/mpitutorial/blob/gh-pages/tut
 */
 
 #include <mpi.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DATA_SOURCE_RANK 0
+#define DATA_DESTINATION_RANK 1
+
+typedef struct
+{
+  int64_t a;
+  int64_t b;
+  int64_t c;
+  int64_t d;
+} Record;
+
+const int32_t MB = 1 << 20;
 
 int main(int argc, char **argv)
 {
@@ -14,8 +28,8 @@ int main(int argc, char **argv)
   MPI_Init(NULL, NULL);
 
   // Find out rank, size
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -26,52 +40,104 @@ int main(int argc, char **argv)
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  int num_of_ints = 40000;
+  // Get the name of the processor, to be printed later
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  // Construct MPI's version of type Record
+  int count = 4;
+  int block_lengths[4] = {1, 1, 1, 1};
+  MPI_Aint offsets[4];
+  offsets[0] = offsetof(Record, a);
+  offsets[1] = offsetof(Record, b);
+  offsets[2] = offsetof(Record, c);
+  offsets[3] = offsetof(Record, d);
+  MPI_Datatype types[4] = {MPI_INT64_T, MPI_INT64_T, MPI_INT64_T, MPI_INT64_T};
+  MPI_Datatype MPI_Record;
+
+  MPI_Type_create_struct(count,
+                         block_lengths,
+                         offsets,
+                         types,
+                         &MPI_Record);
+  MPI_Type_commit(&MPI_Record);
+
+  int MPI_Record_size;
+  MPI_Type_size(MPI_Record, &MPI_Record_size);
+
+  // Will be used for timing
   double t1, t2;
 
-  if (world_rank == 0)
+  char *temp = argv[1];
+  int num_of_records = atoi(temp);
+
+  if (rank == DATA_SOURCE_RANK)
   {
-    int *int_arr = (int *)malloc(num_of_ints * sizeof(int));
+    Record *src_arr = malloc(num_of_records * sizeof(Record));
 
     int i;
-    for (i = 0; i < num_of_ints; i++)
+    for (i = 0; i < num_of_records; i++)
     {
-      int_arr[i] = i;
+      src_arr[i] = (Record){42, 42, 42, 42};
     }
 
     t1 = MPI_Wtime();
 
     MPI_Send(
-        /* data         = */ int_arr,
-        /* count        = */ num_of_ints,
-        /* datatype     = */ MPI_INT,
-        /* destination  = */ 1,
+        /* data         = */ src_arr,
+        /* count        = */ num_of_records,
+        /* datatype     = */ MPI_Record,
+        /* destination  = */ DATA_DESTINATION_RANK,
         /* tag          = */ 0,
         /* communicator = */ MPI_COMM_WORLD);
 
     t2 = MPI_Wtime();
 
-    printf("Sending took %f \n", t2 - t1);
+    printf("Sending took %f on %s \n", t2 - t1, processor_name);
+    printf("Sent data in bytes %d \n", num_of_records * MPI_Record_size);
+    printf("Speed of data sending %f (MB/s)\n", num_of_records * MPI_Record_size / MB / (t2 - t1));
+
+    FILE *fp;
+    fp = fopen("MPI_send_recv_records_SEND.log", "a");
+    fprintf(fp, "%d \t %f \n", num_of_records, num_of_records * MPI_Record_size / MB / (t2 - t1));
+    fclose(fp);
   }
 
-  else if (world_rank == 1)
+  else if (rank == DATA_DESTINATION_RANK)
   {
-    int *int_arr = (int *)malloc(num_of_ints * sizeof(int));
+    Record *recv_arr = malloc(num_of_records * sizeof(Record));
 
     t1 = MPI_Wtime();
 
     MPI_Recv(
-        /* data         = */ int_arr,
-        /* count        = */ num_of_ints,
-        /* datatype     = */ MPI_INT,
-        /* source       = */ 0,
+        /* data         = */ recv_arr,
+        /* count        = */ num_of_records,
+        /* datatype     = */ MPI_Record,
+        /* source       = */ DATA_SOURCE_RANK,
         /* tag          = */ 0,
         /* communicator = */ MPI_COMM_WORLD,
         /* status       = */ MPI_STATUS_IGNORE);
 
     t2 = MPI_Wtime();
 
-    printf("Receiving took %f \n", t2 - t1);
+    printf("Receiving took %f on %s \n", t2 - t1, processor_name);
+
+    if (recv_arr[num_of_records - 1].c == 42)
+    {
+      printf("Verification of sample data: pass \n");
+      printf("Received data in bytes %d \n", num_of_records * MPI_Record_size);
+      printf("Speed of data receiving %f (MB/s)\n", num_of_records * MPI_Record_size / MB / (t2 - t1));
+
+      FILE *fp;
+      fp = fopen("MPI_send_recv_records_RECV.log", "a");
+      fprintf(fp, "%d \t %f \n", num_of_records, num_of_records * MPI_Record_size / MB / (t2 - t1));
+      fclose(fp);
+    }
+    else
+    {
+      printf("Verification of sample data: failed \n");
+    }
   }
 
   MPI_Finalize();
