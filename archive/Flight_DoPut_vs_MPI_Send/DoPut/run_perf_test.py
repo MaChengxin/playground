460ff56051fd3e436cc63e63e306b287707f83b1
macx <cxma@protonmail.ch>
2019-12-17 18:27:38 +0100

    Add results of experiments on Cartesius

---
+++ archive/Flight_DoPut_vs_MPI_Send/DoPut/run_perf_test.py
@@ -0,0 +1,13 @@
+import subprocess
+KILO = 1000
+
+for num_of_records in range(100*KILO, 4000*KILO, 200*KILO):
+    for _ in range(10):
+        subprocess.call(["./arrow-flight-benchmark",
+                            "--server_host", "localhost",
+                            "--records_per_stream", str(num_of_records),
+                            "--records_per_batch", str(16384),
+                            "--num_streams", str(1),
+                            "--num_threads", str(1),
+                            "--test_put"
+                            ])
