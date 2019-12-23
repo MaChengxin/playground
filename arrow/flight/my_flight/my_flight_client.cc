2bdb3a610c4a41c2a26b565c6371ba74c05dafbe
macx <cxma@protonmail.ch>
2019-12-23 11:53:04 +0100

    Finished making a Record Batch and sending it to the server

--- arrow/flight/my_flight/my_flight_client.cc
+++ arrow/flight/my_flight/my_flight_client.cc
@@ -1,7 +1,91 @@
-/* 
+/*
 Assuming one client and one server now.
-In the final implementation, there will be more nodes and each node is both a client and a server (all-to-all communication pattern).
-Step 1: send "Hello World" to the server
-Step 2: make a Record Batch, send it to the server
-Step 3: sort the Record Batch according to some criteria, send the second half to the server
+In the final implementation, there will be more nodes and each node is both a client and a
+server (all-to-all communication pattern).
+TODO: sort the Record Batch according to some criteria, send the second half to the
+server
 */
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <gflags/gflags.h>
+
+#include "arrow/api.h"
+#include "arrow/builder.h"
+#include "arrow/flight/api.h"
+#include "arrow/record_batch.h"
+#include "arrow/testing/gtest_util.h"
+
+DEFINE_string(server_host, "localhost", "Host where the server is running on");
+DEFINE_int32(server_port, 30103, "The port to connect to");
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  std::unique_ptr<arrow::flight::FlightClient> client;
+  arrow::flight::Location location;
+  ABORT_NOT_OK(arrow::flight::Location::ForGrpcTcp(FLAGS_server_host, FLAGS_server_port,
+                                                   &location));
+  std::cout << "Server location: " << location.ToString() << std::endl;
+  ABORT_NOT_OK(arrow::flight::FlightClient::Connect(location, &client));
+
+  std::cout << "Goed, connection didn't fail. \n " << std::endl;
+
+  std::unique_ptr<arrow::flight::FlightStreamWriter> writer;
+  std::unique_ptr<arrow::flight::FlightMetadataReader> reader;
+  std::shared_ptr<arrow::Schema> schema = arrow::schema(
+      {arrow::field("a", arrow::int64()), arrow::field("b", arrow::int64()),
+       arrow::field("c", arrow::int64()), arrow::field("d", arrow::int64())});
+  arrow::Status do_put_status =
+      client->DoPut(arrow::flight::FlightDescriptor{}, schema, &writer, &reader);
+  if (!do_put_status.ok()) {
+    std::cerr << "Client DoPut failed with error: << " << do_put_status.ToString()
+              << std::endl;
+  }
+
+  const int64_t batch_num_rows = 100;
+
+  int64_t raw_array[batch_num_rows];
+  for (int i = 0; i < batch_num_rows; ++i) {
+    raw_array[i] = i;
+  }
+
+  arrow::Int64Builder builder;
+
+  ABORT_NOT_OK(builder.AppendValues(raw_array, batch_num_rows, nullptr));
+
+  std::shared_ptr<arrow::Int64Array> batch_col;
+  ABORT_NOT_OK(builder.Finish(&batch_col));
+
+  std::vector<std::shared_ptr<arrow::Array>> batch_cols;
+
+  const int32_t num_cols = 4;
+  arrow::Status make_batch_cols_status;
+  for (int i = 0; i < num_cols; ++i) {
+    batch_cols.push_back(batch_col);
+    make_batch_cols_status = batch_cols.back()->Validate();
+  }
+
+  if (!make_batch_cols_status.ok()) {
+    std::cerr << "Making columns of the Record Batch failed with error: << "
+              << make_batch_cols_status.ToString() << std::endl;
+  }
+
+  std::shared_ptr<arrow::RecordBatch> record_batch =
+      arrow::RecordBatch::Make(schema, batch_num_rows, batch_cols);
+
+  // TODO: verify the content of the Record Batch
+
+  arrow::Status writer_status;
+
+  writer_status = writer->WriteRecordBatch(*record_batch);
+  writer_status = writer->Close();
+
+  if (!writer_status.ok()) {
+    std::cerr << "Writer failed with error: << " << writer_status.ToString() << std::endl;
+  }
+
+  return 0;
+}
--- arrow/flight/my_flight/my_flight_server.cc
+++ arrow/flight/my_flight/my_flight_server.cc
@@ -1,52 +1,53 @@
-// #include "arrow/api.h"
-// #include "arrow/ipc/api.h"
-
 #include <iostream>
 #include <memory>
 
 #include <gflags/gflags.h>
 
 #include "arrow/flight/api.h"
-// #include "arrow/util/logging.h"
+#include "arrow/util/logging.h"
 
 DEFINE_string(server_host, "localhost", "Host where the server is running on");
 DEFINE_int32(server_port, 30103, "Server port to listen on");
 
-class MyFlightServer : public arrow::flight::FlightServerBase
-{
-    arrow::Status DoPut(const arrow::flight::ServerCallContext &context,
-                        std::unique_ptr<arrow::flight::FlightMessageReader> reader,
-                        std::unique_ptr<arrow::flight::FlightMetadataWriter> writer) override
-    {
-        arrow::flight::FlightStreamChunk chunk;
-        while (true)
-        {
-            RETURN_NOT_OK(reader->Next(&chunk));
-            if (!chunk.data)
-                break;
-            if (chunk.app_metadata)
-            {
-                RETURN_NOT_OK(writer->WriteMetadata(*chunk.app_metadata));
-            }
-        }
-        return arrow::Status::OK();
+class MyFlightServer : public arrow::flight::FlightServerBase {
+  arrow::Status DoPut(
+      const arrow::flight::ServerCallContext& context,
+      std::unique_ptr<arrow::flight::FlightMessageReader> reader,
+      std::unique_ptr<arrow::flight::FlightMetadataWriter> writer) override {
+    arrow::flight::FlightStreamChunk chunk;
+    while (true) {
+      RETURN_NOT_OK(reader->Next(&chunk));
+      if (!chunk.data) break;
+      if (chunk.app_metadata) {
+        RETURN_NOT_OK(writer->WriteMetadata(*chunk.app_metadata));
+      }
     }
+
+    std::cout << "Server says everything looks OK! " << std::endl;
+
+    return arrow::Status::OK();
+  }
 };
 
-int main(int argc, char **argv)
-{
-    std::unique_ptr<MyFlightServer> my_flight_server;
-    my_flight_server.reset(new MyFlightServer);
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    // arrow::flight::Location location;
-    // ARROW_CHECK_OK(arrow::flight::Location::ForGrpcTcp("0.0.0.0", FLAGS_server_port, &location));
-    // arrow::flight::FlightServerOptions options(location);
+  std::unique_ptr<MyFlightServer> my_flight_server;
+  my_flight_server.reset(new MyFlightServer);
 
-    // ARROW_CHECK_OK(my_flight_server->Init(options));
-    // // Exit with a clean error code (0) on SIGTERM
-    // ARROW_CHECK_OK(my_flight_server->SetShutdownOnSignals({SIGTERM}));
-    // std::cout << "Server host: " << FLAGS_server_host << std::endl;
-    // std::cout << "Server port: " << FLAGS_server_port << std::endl;
-    // ARROW_CHECK_OK(my_flight_server->Serve());
-    return 0;
+  // According to Flight.proto: a location is where a Flight service will accept
+  // retrieval of a particular stream given a ticket.
+  arrow::flight::Location location;
+  ARROW_CHECK_OK(
+      arrow::flight::Location::ForGrpcTcp("0.0.0.0", FLAGS_server_port, &location));
+  std::cout << "Server location: " << location.ToString() << std::endl;
+  arrow::flight::FlightServerOptions options(location);
+
+  ARROW_CHECK_OK(my_flight_server->Init(options));
+  // Exit with a clean error code (0) on SIGTERM
+  ARROW_CHECK_OK(my_flight_server->SetShutdownOnSignals({SIGTERM}));
+  std::cout << "Server host: " << FLAGS_server_host << std::endl;
+  std::cout << "Server port: " << FLAGS_server_port << std::endl;
+  ARROW_CHECK_OK(my_flight_server->Serve());
+  return 0;
 }
