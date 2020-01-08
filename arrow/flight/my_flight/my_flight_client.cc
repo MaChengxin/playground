eeb0ab37e164938b8e8bd0c084f9b2585462f83d
macx <cxma@protonmail.ch>
2020-01-08 15:28:30 +0100

    Retrieve Record Batch from Plasma instead of making MyRecordBatch

--- arrow/flight/my_flight/my_flight_client.cc
+++ arrow/flight/my_flight/my_flight_client.cc
@@ -8,21 +8,23 @@ This client is capable of sending record batches to multiple servers.
 
 #include <gflags/gflags.h>
 
-#include "arrow/api.h"
-#include "arrow/builder.h"
-#include "arrow/flight/api.h"
-#include "arrow/record_batch.h"
-#include "arrow/testing/gtest_util.h"
+#include <arrow/api.h>
+#include <arrow/builder.h>
+#include <arrow/flight/api.h>
+#include <arrow/io/memory.h>
+#include <arrow/ipc/reader.h>
+#include <arrow/record_batch.h>
+#include <arrow/testing/gtest_util.h>
+#include <arrow/util/logging.h>
+#include <plasma/client.h>
 
 DEFINE_string(server_hosts, "localhost", "Host(s) where the server(s) is/are running on");
 DEFINE_int32(server_port, 30103, "The port to connect to");
 
 std::shared_ptr<arrow::RecordBatch> MakeMyRecordBatch(
-    const std::shared_ptr<arrow::Schema> &schema, int64_t batch_num_rows)
-{
+    const std::shared_ptr<arrow::Schema>& schema, int64_t batch_num_rows) {
   int64_t raw_array[batch_num_rows];
-  for (int i = 0; i < batch_num_rows; ++i)
-  {
+  for (int i = 0; i < batch_num_rows; ++i) {
     raw_array[i] = i;
   }
 
@@ -36,14 +38,12 @@ std::shared_ptr<arrow::RecordBatch> MakeMyRecordBatch(
 
   const int32_t num_cols = schema->num_fields();
   arrow::Status make_batch_cols_status;
-  for (int i = 0; i < num_cols; ++i)
-  {
+  for (int i = 0; i < num_cols; ++i) {
     batch_cols.push_back(batch_col);
     make_batch_cols_status = batch_cols.back()->Validate();
   }
 
-  if (!make_batch_cols_status.ok())
-  {
+  if (!make_batch_cols_status.ok()) {
     std::cerr << "Making columns of the Record Batch failed with error: << "
               << make_batch_cols_status.ToString() << std::endl;
   }
@@ -55,8 +55,7 @@ std::shared_ptr<arrow::RecordBatch> MakeMyRecordBatch(
 }
 
 arrow::Status SendToResponsibleNode(std::string host, int port,
-                                    const arrow::RecordBatch &record_batch)
-{
+                                    const arrow::RecordBatch& record_batch) {
   std::unique_ptr<arrow::flight::FlightClient> client;
   arrow::flight::Location location;
   ABORT_NOT_OK(arrow::flight::Location::ForGrpcTcp(host, port, &location));
@@ -67,16 +66,14 @@ arrow::Status SendToResponsibleNode(std::string host, int port,
 
   arrow::Status do_put_status = client->DoPut(arrow::flight::FlightDescriptor{},
                                               record_batch.schema(), &writer, &reader);
-  if (!do_put_status.ok())
-  {
+  if (!do_put_status.ok()) {
     return do_put_status;
   }
 
   arrow::Status writer_status;
   writer_status = writer->WriteRecordBatch(record_batch);
   writer_status = writer->Close();
-  if (!writer_status.ok())
-  {
+  if (!writer_status.ok()) {
     return writer_status;
   }
 
@@ -85,16 +82,14 @@ arrow::Status SendToResponsibleNode(std::string host, int port,
 
 /* Source:
  * https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c*/
-std::vector<std::string> SeparateServerHosts(std::string server_hosts)
-{
+std::vector<std::string> SeparateServerHosts(std::string server_hosts) {
   std::string s = server_hosts;
   std::string delimiter = ",";
   std::vector<std::string> server_hosts_vec;
 
   size_t pos = 0;
   std::string token;
-  while ((pos = s.find(delimiter)) != std::string::npos)
-  {
+  while ((pos = s.find(delimiter)) != std::string::npos) {
     token = s.substr(0, pos);
     server_hosts_vec.push_back(token);
     s.erase(0, pos + delimiter.length());
@@ -103,45 +98,62 @@ std::vector<std::string> SeparateServerHosts(std::string server_hosts)
   return server_hosts_vec;
 }
 
-int main(int argc, char **argv)
-{
+void GetRecordBatchFromPlasma(plasma::ObjectID object_id,
+                              std::shared_ptr<arrow::RecordBatch>& record_batch) {}
+
+int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  std::shared_ptr<arrow::Schema> schema =
-      arrow::schema({arrow::field("a", arrow::int64()), arrow::field("b", arrow::int64()),
-                     arrow::field("c", arrow::int64())});
+  /* NOTE: putting the functionality of retrieving Record Batch from Plasma in a function
+   * leads to problem which I am unable to solve at the moment. The problem is that by
+   * doing that, the meta info of the retrieved Record Batch is readable, but the content
+   * (i.e. the values in the columns) is not available and the program terminates with a
+   * segmentation fault. */
+  /* === Start retrieving Record Batch from Plasma === */
+  // Start up and connect a Plasma client
+  plasma::PlasmaClient client;
+  ARROW_CHECK_OK(client.Connect("/tmp/plasma"));
+  plasma::ObjectID object_id = plasma::ObjectID::from_binary("0FF1CE00C0FFEE00BEEF");
+  plasma::ObjectBuffer object_buffer;
+  ARROW_CHECK_OK(client.Get(&object_id, 1, -1, &object_buffer));
 
-  const int64_t batch_num_rows = 720;
+  // Retrieve object data
+  auto buffer = object_buffer.data;
 
-  std::shared_ptr<arrow::RecordBatch> record_batch =
-      MakeMyRecordBatch(schema, batch_num_rows);
+  arrow::io::BufferReader buffer_reader(
+      buffer);  // BufferReader is a derived class of arrow::io::InputStream
+  std::shared_ptr<arrow::ipc::RecordBatchReader> record_batch_stream_reader;
+  ARROW_CHECK_OK(arrow::ipc::RecordBatchStreamReader::Open(&buffer_reader,
+                                                           &record_batch_stream_reader));
 
-  std::cout << "Column 2 of the full Record Batch: \n"
-            << record_batch->column(2)->ToString() << std::endl;
-  std::cout << "Number of columns of the full Record Batch: "
-            << record_batch->num_columns() << std::endl;
-  std::cout << "Number of rows of the full Record Batch: " << record_batch->num_rows()
-            << std::endl;
-  std::cout << "Schema of the full Record Batch: \n"
+  std::shared_ptr<arrow::RecordBatch> record_batch;
+  arrow::Status status;
+  status = record_batch_stream_reader->ReadNext(&record_batch);
+  /* === End retrieving Record Batch from Plasma === */
+
+  std::cout << "Schema of the retrieved Record Batch: \n"
             << record_batch->schema()->ToString() << std::endl;
+  std::cout << "Number of columns of the retrieved Record Batch: "
+            << record_batch->num_columns() << std::endl;
+  std::cout << "Number of rows of the retrieved Record Batch: "
+            << record_batch->num_rows() << std::endl;
+  std::cout << "Column 0 of the retrieved Record Batch: \n"
+            << record_batch->column(0)->ToString() << std::endl;
+  std::cout << "Column 1 of the retrieved Record Batch: \n"
+            << record_batch->column(1)->ToString() << std::endl;
+  std::cout << "Column 2 of the retrieved Record Batch: \n"
+            << record_batch->column(2)->ToString() << std::endl;
 
   arrow::Status comm_status;
   std::vector<std::string> server_hosts = SeparateServerHosts(FLAGS_server_hosts);
-  int num_rows_to_per_node = batch_num_rows / server_hosts.size();
-  int start_row_index = 0;
 
   // TODO: make this parallelized
-  for (auto const &server_host : server_hosts)
-  {
-    comm_status = SendToResponsibleNode(
-        server_host, FLAGS_server_port,
-        *record_batch->Slice(start_row_index, num_rows_to_per_node));
-    if (!comm_status.ok())
-    {
-      std::cerr << "Sending to responsible node failed with error: << "
+  for (auto const& server_host : server_hosts) {
+    comm_status = SendToResponsibleNode(server_host, FLAGS_server_port, *record_batch);
+    if (!comm_status.ok()) {
+      std::cerr << "Sending to responsible node failed with error: "
                 << comm_status.ToString() << std::endl;
     }
-    start_row_index += num_rows_to_per_node;
   }
 
   return 0;
--- arrow/flight/my_flight/sort.cc
+++ arrow/flight/my_flight/sort.cc
@@ -17,161 +17,172 @@
 
 DEFINE_string(input_file, "", "The input file containing data to be sorted.");
 
-struct Record
-{
-    std::string group_name;
-    int64_t seq;
-    std::string data;
+struct Record {
+  std::string group_name;
+  int64_t seq;
+  std::string data;
 };
 
-bool CompareGroupName(const std::string &a, const std::string &b)
-{
-    std::string group_str = "GROUP";
-    std::string a_substr = a.substr(group_str.length());
-    int a_substr_int = std::stoi(a_substr);
-    std::string b_substr = b.substr(group_str.length());
-    int b_substr_int = std::stoi(b_substr);
-    return a_substr_int < b_substr_int;
+bool CompareGroupName(const std::string& a, const std::string& b) {
+  std::string group_str = "GROUP";
+  std::string a_substr = a.substr(group_str.length());
+  int a_substr_int = std::stoi(a_substr);
+  std::string b_substr = b.substr(group_str.length());
+  int b_substr_int = std::stoi(b_substr);
+  return a_substr_int < b_substr_int;
 }
 
-/* Source: https://stackoverflow.com/questions/3574680/sort-based-on-multiple-things-in-c*/
-bool CompareRecords(const Record &a, const Record &b)
-{
-    if (CompareGroupName(a.group_name, b.group_name))
-        return true;
-    if (CompareGroupName(b.group_name, a.group_name))
-        return false;
+/* Source:
+ * https://stackoverflow.com/questions/3574680/sort-based-on-multiple-things-in-c*/
+bool CompareRecords(const Record& a, const Record& b) {
+  if (CompareGroupName(a.group_name, b.group_name)) return true;
+  if (CompareGroupName(b.group_name, a.group_name)) return false;
 
-    // a == b for primary condition, go to secondary condition
-    if (a.seq < b.seq)
-        return true;
-    if (b.seq < a.seq)
-        return false;
+  // a == b for primary condition, go to secondary condition
+  if (a.seq < b.seq) return true;
+  if (b.seq < a.seq) return false;
 
-    return false;
+  return false;
 }
 
-std::shared_ptr<arrow::RecordBatch> ConvertStructVectorToRecordBatch(const std::vector<Record> &records)
-{
-    // Hard-coded for now; can be derived from struct Record
-    std::shared_ptr<arrow::Schema> schema = arrow::schema({arrow::field("group_name", arrow::utf8()),
-                                                           arrow::field("seq", arrow::int64()),
-                                                           arrow::field("data", arrow::utf8())});
+std::shared_ptr<arrow::RecordBatch> ConvertStructVectorToRecordBatch(
+    const std::vector<Record>& records) {
+  // Hard-coded for now; can be derived from struct Record
+  std::shared_ptr<arrow::Schema> schema = arrow::schema(
+      {arrow::field("group_name", arrow::utf8()), arrow::field("seq", arrow::int64()),
+       arrow::field("data", arrow::utf8())});
 
-    int64_t records_size = static_cast<int64_t>(records.size());
+  int64_t records_size = static_cast<int64_t>(records.size());
 
-    std::vector<std::string> group_name_vector;
-    int64_t seq_array[records_size];
-    std::vector<std::string> data_vector;
+  std::vector<std::string> group_name_vector;
+  int64_t seq_array[records_size];
+  std::vector<std::string> data_vector;
 
-    for (int64_t i = 0; i < records_size; ++i)
-    {
-        group_name_vector.push_back(records[i].group_name);
-        seq_array[i] = records[i].seq;
-        data_vector.push_back(records[i].data);
-    }
+  for (int64_t i = 0; i < records_size; ++i) {
+    group_name_vector.push_back(records[i].group_name);
+    seq_array[i] = records[i].seq;
+    data_vector.push_back(records[i].data);
+  }
 
-    arrow::StringBuilder group_name_builder;
-    arrow::Int64Builder seq_builder;
-    arrow::StringBuilder data_builder;
-    ABORT_NOT_OK(group_name_builder.AppendValues(group_name_vector));
-    ABORT_NOT_OK(seq_builder.AppendValues(seq_array, records_size));
-    ABORT_NOT_OK(data_builder.AppendValues(data_vector));
+  arrow::StringBuilder group_name_builder;
+  arrow::Int64Builder seq_builder;
+  arrow::StringBuilder data_builder;
+  ABORT_NOT_OK(group_name_builder.AppendValues(group_name_vector));
+  ABORT_NOT_OK(seq_builder.AppendValues(seq_array, records_size));
+  ABORT_NOT_OK(data_builder.AppendValues(data_vector));
 
-    std::shared_ptr<arrow::StringArray> group_name_col;
-    std::shared_ptr<arrow::Int64Array> seq_col;
-    std::shared_ptr<arrow::StringArray> data_col;
-    ABORT_NOT_OK(group_name_builder.Finish(&group_name_col));
-    ABORT_NOT_OK(seq_builder.Finish(&seq_col));
-    ABORT_NOT_OK(data_builder.Finish(&data_col));
+  std::shared_ptr<arrow::StringArray> group_name_col;
+  std::shared_ptr<arrow::Int64Array> seq_col;
+  std::shared_ptr<arrow::StringArray> data_col;
+  ABORT_NOT_OK(group_name_builder.Finish(&group_name_col));
+  ABORT_NOT_OK(seq_builder.Finish(&seq_col));
+  ABORT_NOT_OK(data_builder.Finish(&data_col));
 
-    std::vector<std::shared_ptr<arrow::Array>> batch_cols;
-    batch_cols.push_back(group_name_col);
-    batch_cols.push_back(seq_col);
-    batch_cols.push_back(data_col);
+  std::vector<std::shared_ptr<arrow::Array>> batch_cols;
+  batch_cols.push_back(group_name_col);
+  batch_cols.push_back(seq_col);
+  batch_cols.push_back(data_col);
 
-    std::shared_ptr<arrow::RecordBatch> record_batch =
-        arrow::RecordBatch::Make(schema, records_size, batch_cols);
+  std::shared_ptr<arrow::RecordBatch> record_batch =
+      arrow::RecordBatch::Make(schema, records_size, batch_cols);
 
-    std::cout << "Schema of the converted Record Batch: \n"
-              << record_batch->schema()->ToString() << std::endl;
-    std::cout << "Number of columns of the converted Record Batch: " << record_batch->num_columns() << std::endl;
-    std::cout << "Number of rows of the converted Record Batch: " << record_batch->num_rows() << std::endl;
-    std::cout << "Column 0 of the converted Record Batch: \n"
-              << record_batch->column(0)->ToString() << std::endl;
-    std::cout << "Column 1 of the converted Record Batch: \n"
-              << record_batch->column(1)->ToString() << std::endl;
-    std::cout << "Column 2 of the converted Record Batch: \n"
-              << record_batch->column(2)->ToString() << std::endl;
+  std::cout << "Schema of the converted Record Batch: \n"
+            << record_batch->schema()->ToString() << std::endl;
+  std::cout << "Number of columns of the converted Record Batch: "
+            << record_batch->num_columns() << std::endl;
+  std::cout << "Number of rows of the converted Record Batch: "
+            << record_batch->num_rows() << std::endl;
+  std::cout << "Column 0 of the converted Record Batch: \n"
+            << record_batch->column(0)->ToString() << std::endl;
+  std::cout << "Column 1 of the converted Record Batch: \n"
+            << record_batch->column(1)->ToString() << std::endl;
+  std::cout << "Column 2 of the converted Record Batch: \n"
+            << record_batch->column(2)->ToString() << std::endl;
 
-    return record_batch;
+  return record_batch;
 }
 
-plasma::ObjectID PutRecordVectorToPlasmaStore(const std::vector<Record> &records)
-{
-    // Convert vector of records to a Record Batch
-    std::shared_ptr<arrow::RecordBatch> record_batch = ConvertStructVectorToRecordBatch(records);
+plasma::ObjectID PutRecordVectorToPlasmaStore(const std::vector<Record>& records) {
+  // Convert vector of records to a Record Batch
+  std::shared_ptr<arrow::RecordBatch> record_batch =
+      ConvertStructVectorToRecordBatch(records);
 
-    // Get size of the Record Batch
-    arrow::io::MockOutputStream mock_output_stream;
-    std::shared_ptr<arrow::ipc::RecordBatchWriter> record_batch_writer;
-    arrow::Status status;
-    status = arrow::ipc::RecordBatchStreamWriter::Open(&mock_output_stream, record_batch->schema(), &record_batch_writer);
-    status = record_batch_writer->WriteRecordBatch(*record_batch);
-    int64_t data_size = mock_output_stream.GetExtentBytesWritten();
+  // Get size of the Record Batch
+  arrow::io::MockOutputStream mock_output_stream;
+  std::shared_ptr<arrow::ipc::RecordBatchWriter> record_batch_writer;
+  arrow::Status status;
+  status = arrow::ipc::RecordBatchStreamWriter::Open(
+      &mock_output_stream, record_batch->schema(), &record_batch_writer);
+  if (!status.ok()) {
+    std::cerr << "arrow::ipc::RecordBatchStreamWriter::Open failed with error: "
+              << status.ToString() << std::endl;
+  }
+  status = record_batch_writer->WriteRecordBatch(*record_batch);
+  if (!status.ok()) {
+    std::cerr << "record_batch_writer->WriteRecordBatch failed with error: "
+              << status.ToString() << std::endl;
+  }
+  int64_t data_size = mock_output_stream.GetExtentBytesWritten();
 
-    // Start up and connect a Plasma client
-    plasma::PlasmaClient client;
-    ARROW_CHECK_OK(client.Connect("/tmp/plasma"));
+  // Start up and connect a Plasma client
+  plasma::PlasmaClient client;
+  ARROW_CHECK_OK(client.Connect("/tmp/plasma"));
 
-    // Create an object with a fixed ObjectID
-    plasma::ObjectID object_id = plasma::ObjectID::from_binary("0FF1CE00C0FFEE00BEEF");
-    std::shared_ptr<Buffer> buf;
-    ARROW_CHECK_OK(client.Create(object_id, data_size, nullptr, 0, &buf));
+  // Create an object with a fixed ObjectID
+  plasma::ObjectID object_id = plasma::ObjectID::from_binary("0FF1CE00C0FFEE00BEEF");
+  std::shared_ptr<Buffer> buf;
+  ARROW_CHECK_OK(client.Create(object_id, data_size, nullptr, 0, &buf));
 
-    // Write the Record Batch into the object
-    arrow::io::FixedSizeBufferWriter buffer_writer(buf);
-    status = arrow::ipc::RecordBatchStreamWriter::Open(&buffer_writer, record_batch->schema(), &record_batch_writer);
-    status = record_batch_writer->WriteRecordBatch(*record_batch);
+  // Write the Record Batch into the object
+  arrow::io::FixedSizeBufferWriter buffer_writer(buf);
+  status = arrow::ipc::RecordBatchStreamWriter::Open(
+      &buffer_writer, record_batch->schema(), &record_batch_writer);
+  if (!status.ok()) {
+    std::cerr << "arrow::ipc::RecordBatchStreamWriter::Open failed with error: "
+              << status.ToString() << std::endl;
+  }
+  status = record_batch_writer->WriteRecordBatch(*record_batch);
+  if (!status.ok()) {
+    std::cerr << "record_batch_writer->WriteRecordBatch failed with error: "
+              << status.ToString() << std::endl;
+  }
 
-    // Seal the object
-    ARROW_CHECK_OK(client.Seal(object_id));
+  // Seal the object
+  ARROW_CHECK_OK(client.Seal(object_id));
 
-    // Disconnect the client
-    ARROW_CHECK_OK(client.Disconnect());
+  // Disconnect the client
+  ARROW_CHECK_OK(client.Disconnect());
 
-    return object_id;
+  return object_id;
 }
 
-int main(int argc, char **argv)
-{
-    // Read the input file, construct the data to be sorted
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+int main(int argc, char** argv) {
+  // Read the input file, construct the data to be sorted
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-    std::vector<Record> records;
+  std::vector<Record> records;
 
-    std::ifstream in_file(FLAGS_input_file);
-    std::string group, seq, data;
-    while (in_file >> group >> seq >> data)
-    {
-        records.push_back({group, std::stoi(seq), data});
-    }
+  std::ifstream in_file(FLAGS_input_file);
+  std::string group, seq, data;
+  while (in_file >> group >> seq >> data) {
+    records.push_back({group, std::stoi(seq), data});
+  }
 
-    std::cout << "Before sorting" << std::endl;
-    for (auto const &rec : records)
-    {
-        std::cout << rec.group_name << "\t" << rec.seq << "\t" << rec.data << std::endl;
-    }
+  std::cout << "Before sorting" << std::endl;
+  for (auto const& rec : records) {
+    std::cout << rec.group_name << "\t" << rec.seq << "\t" << rec.data << std::endl;
+  }
 
-    // Sort the data
-    std::sort(records.begin(), records.end(), CompareRecords);
-    std::cout << "After sorting" << std::endl;
-    for (auto const &rec : records)
-    {
-        std::cout << rec.group_name << "\t" << rec.seq << "\t" << rec.data << std::endl;
-    }
+  // Sort the data
+  std::sort(records.begin(), records.end(), CompareRecords);
+  std::cout << "After sorting" << std::endl;
+  for (auto const& rec : records) {
+    std::cout << rec.group_name << "\t" << rec.seq << "\t" << rec.data << std::endl;
+  }
 
-    // Put into Plasma Object Store, to be used by Flight Client
-    plasma::ObjectID object_id;
-    object_id = PutRecordVectorToPlasmaStore(records);
+  // Put into Plasma Object Store, to be used by Flight Client
+  plasma::ObjectID object_id;
+  object_id = PutRecordVectorToPlasmaStore(records);
+
+  // TODO: partition the records before putting into Plasma
 }
