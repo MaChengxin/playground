74631e688fdcbc6baba0773b5ca4fef8269e9366
macx <cxma@protonmail.ch>
2020-01-09 19:25:16 +0100

    Keep the Plasma client alive before the Record Batch is retrieved

--- arrow/sam/retrieve.cc
+++ arrow/sam/retrieve.cc
@@ -7,16 +7,12 @@
 #include <arrow/util/logging.h>
 #include <plasma/client.h>
 
-std::shared_ptr<arrow::RecordBatch> GetRecordBatchFromPlasma(plasma::ObjectID object_id)
+std::shared_ptr<arrow::RecordBatch> GetRecordBatchFromPlasma(plasma::ObjectID object_id,
+                                                             plasma::PlasmaClient &client)
 {
-    // Start up and connect a Plasma client.
-    plasma::PlasmaClient client;
-    ARROW_CHECK_OK(client.Connect("/tmp/store"));
-
     plasma::ObjectBuffer object_buffer;
     ARROW_CHECK_OK(client.Get(&object_id, 1, -1, &object_buffer));
 
-    // Retrieve object data.
     auto buffer = object_buffer.data;
 
     arrow::io::BufferReader buffer_reader(buffer);
@@ -26,9 +22,6 @@ std::shared_ptr<arrow::RecordBatch> GetRecordBatchFromPlasma(plasma::ObjectID ob
     std::shared_ptr<arrow::RecordBatch> record_batch;
     arrow::Status status = record_batch_stream_reader->ReadNext(&record_batch);
 
-    // Disconnect the client.
-    ARROW_CHECK_OK(client.Disconnect());
-
     return record_batch;
 }
 
@@ -36,31 +29,15 @@ int main(int argc, char **argv)
 {
     plasma::ObjectID object_id = plasma::ObjectID::from_binary("0FF1CE00C0FFEE00BEEF");
 
-    // Start up and connect a Plasma client.
+    // Start up and connect a Plasma client
     plasma::PlasmaClient client;
     ARROW_CHECK_OK(client.Connect("/tmp/store"));
 
-    plasma::ObjectBuffer object_buffer;
-    ARROW_CHECK_OK(client.Get(&object_id, 1, -1, &object_buffer));
+    std::shared_ptr<arrow::RecordBatch> record_batch = GetRecordBatchFromPlasma(object_id, client);
 
-    // Retrieve object data.
-    auto buffer = object_buffer.data;
+    // Disconnect the Plasma client
+    ARROW_CHECK_OK(client.Disconnect());
 
-    arrow::io::BufferReader buffer_reader(buffer); // BufferReader is a derived class of arrow::io::InputStream
-    std::shared_ptr<arrow::ipc::RecordBatchReader> record_batch_stream_reader;
-    ARROW_CHECK_OK(arrow::ipc::RecordBatchStreamReader::Open(&buffer_reader, &record_batch_stream_reader));
-
-    std::shared_ptr<arrow::RecordBatch> record_batch;
-    arrow::Status status = record_batch_stream_reader->ReadNext(&record_batch);
-
-    // std::shared_ptr<arrow::RecordBatch> record_batch = GetRecordBatchFromPlasma(object_id);
-
-    std::cout << "record_batch->column_name(0): " << record_batch->column_name(0) << std::endl;
-    std::cout << "record_batch->num_columns(): " << record_batch->num_columns() << std::endl;
-    std::cout << "record_batch->num_rows(): " << record_batch->num_rows() << std::endl;
-
-    std::cout << "record_batch->column(0)->length(): "
-              << record_batch->column(0)->length() << std::endl;
     std::cout << "record_batch->column(0)->ToString(): "
               << record_batch->column(0)->ToString() << std::endl;
 }
