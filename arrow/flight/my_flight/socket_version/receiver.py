8b27e20daefb0dddcf7ba75568ccefe55ebf480e
Chengxin Ma <cxma@protonmail.ch>
2020-01-28 18:08:52 +0100

    Change some terms

--- arrow/flight/my_flight/socket_version/receiver.py
+++ arrow/flight/my_flight/socket_version/receiver.py
@@ -14,7 +14,7 @@ BUFFER_SIZE = 4096
 
 SEPARATOR = "<SEPARATOR>"
 
-# create the server socket
+# create the listening socket
 # TCP socket
 s = socket.socket()
 # bind the socket to our local address
@@ -25,13 +25,13 @@ s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 # accept connection if there is any
-client_socket, address = s.accept() 
+conn, address = s.accept() 
 # if below code is executed, that means the sender is connected
 print(f"[+] {address} is connected.")
 
 # receive the file infos
-# receive using client socket, not server socket
-received = client_socket.recv(BUFFER_SIZE).decode()
+# receive using connection socket, not listening socket
+received = conn.recv(BUFFER_SIZE).decode()
 filename, filesize = received.split(SEPARATOR)
 # remove absolute path if there is
 filename = os.path.basename(filename)
@@ -43,7 +43,7 @@ progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_sc
 with open(filename, "wb") as f:
     for _ in progress:
         # read 1024 bytes from the socket (receive)
-        bytes_read = client_socket.recv(BUFFER_SIZE)
+        bytes_read = conn.recv(BUFFER_SIZE)
         if not bytes_read:    
             # nothing is received
             # file transmitting is done
@@ -53,9 +53,9 @@ with open(filename, "wb") as f:
         # update the progress bar
         progress.update(len(bytes_read))
 
-# close the client socket
-client_socket.close()
-# close the server socket
+# close the connection socket
+conn.close()
+# close the listening socket
 s.close()
         
 
