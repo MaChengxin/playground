f0c76618fd6e27e9d44d6799e2e89bef7a91d0de
Chengxin Ma <cxma@protonmail.ch>
2020-01-29 17:48:35 +0100

    Trivial changes

--- arrow/flight/my_flight/socket_version/sender.py
+++ arrow/flight/my_flight/socket_version/sender.py
@@ -1,5 +1,6 @@
 """
 Client that sends the file (uploads)
+Reference: https://www.thepythoncode.com/article/send-receive-files-using-sockets-python
 """
 import socket
 import tqdm
@@ -24,7 +25,8 @@ def send_file(filename, host, port):
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
     # start sending the file
-    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    progress = tqdm.tqdm(range(
+        filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
     with open(filename, "rb") as f:
         for _ in progress:
             # read the bytes from the file
@@ -32,8 +34,7 @@ def send_file(filename, host, port):
             if not bytes_read:
                 # file transmitting is done
                 break
-            # we use sendall to assure transimission in 
-            # busy networks
+            # we use sendall to assure transimission in busy networks
             s.sendall(bytes_read)
             # update the progress bar
             progress.update(len(bytes_read))
@@ -47,7 +48,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple File Sender")
     parser.add_argument("file", help="File name to send")
     parser.add_argument("host", help="The host/IP address of the receiver")
-    parser.add_argument("-p", "--port", help="Port to use, default is 5001", default=5001)
+    parser.add_argument(
+        "-p", "--port", help="Port to use, default is 5001", default=5001)
     args = parser.parse_args()
     filename = args.file
     host = args.host
