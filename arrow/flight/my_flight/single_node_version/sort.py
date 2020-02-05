d5d4a670cac178163f83fac9258b89534cbab92a
Chengxin Ma <cxma@protonmail.ch>
2020-02-05 14:49:14 +0100

    Add script to handle the special case: 1 node

---
+++ arrow/flight/my_flight/single_node_version/sort.py
@@ -0,0 +1,37 @@
+import argparse
+from datetime import datetime
+import pandas as pd
+import socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_file",
+                    help="The input file containing data to be sorted.")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    with open(socket.gethostname()+'.log', 'a') as f:
+        f.write('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']: ')
+        f.write("started reading input file\n")
+
+    all_records = pd.read_csv(args.input_file,
+                              sep="\t",
+                              names=["group_name", "seq", "data"])
+
+    with open(socket.gethostname()+'.log', 'a') as f:
+        f.write('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']: ')
+        f.write("finished reading input file, started sorting the records\n")
+
+    all_records.sort_values(by=['group_name', 'seq'], inplace=True)
+
+    with open(socket.gethostname()+'.log', 'a') as f:
+        f.write('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']: ')
+        f.write("finished sorting the records, started writing to csv\n")
+
+    all_records.reset_index(drop=True, inplace=True)
+    all_records.to_csv(socket.gethostname()+'.csv', sep='\t', index=False)
+
+    with open(socket.gethostname()+'.log', 'a') as f:
+        f.write('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']: ')
+        f.write("finished writing to csv \n")
