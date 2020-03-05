e23f43753da210ca74340ea573f654b7d20523b1
Chengxin Ma <cxma@protonmail.ch>
2020-03-05 17:20:38 +0100

    Trivial changes

--- distributed_sorting/data/generate_input_data.py
+++ distributed_sorting/data/generate_input_data.py
@@ -1,19 +1,28 @@
+from datetime import datetime
 import random
 
-NUM_OF_NODES = 8
+NUM_OF_NODES = 4
 NUM_OF_GROUPS = 64
-RECORDS_PER_GROUP = 2 * 10**6
+NUM_RECORDS_PER_GROUP = 2 * 10**6
 
-records_per_node = int(NUM_OF_GROUPS*RECORDS_PER_GROUP/NUM_OF_NODES)
+records_per_node = int(NUM_OF_GROUPS*NUM_RECORDS_PER_GROUP/NUM_OF_NODES)
 
-records = [('GROUP'+str(g).zfill(2), str(i).zfill(7), 'DATA'+str(g).zfill(2)+str(i).zfill(7))
-           for g in range(NUM_OF_GROUPS) for i in range(RECORDS_PER_GROUP)]
+records = [("GROUP"+str(g).zfill(2), str(i), "DATA"+str(g).zfill(2)+str(i).zfill(7))
+           for g in range(NUM_OF_GROUPS) for i in range(NUM_RECORDS_PER_GROUP)]
 
-random.shuffle(records)
+# random.shuffle(records)
+
+
+def write_records_to_file(records, filename):
+    with open(filename, "a") as f:
+        for record in records:
+            f.write(record[0] + "\t" + record[1] + "\t" + record[2] + "\n")
+    print("[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+          "]: finished writing to " + filename)
+
 
 for i in range(NUM_OF_NODES):
-    with open('records_on_node_'+str(i)+'.txt', 'a') as f:
-        for j in range(records_per_node):
-            f.write(records[records_per_node*i+j][0]+'\t' +
-                    records[records_per_node*i+j][1]+'\t' +
-                    records[records_per_node*i+j][2]+'\n')
+    print("[" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") +
+          "]: started writing to records_on_node_"+str(i)+".txt")
+    write_records_to_file(records[records_per_node*i:records_per_node*(i+1)],
+                          "expected_records_on_node_"+str(i)+".txt")
