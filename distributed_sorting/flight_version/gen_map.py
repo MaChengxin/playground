33541c384730fe63298ed9f5d521f13c75224148
Chengxin Ma <cxma@protonmail.ch>
2020-03-07 22:17:28 +0100

    Update path to input data

--- distributed_sorting/flight_version/gen_map.py
+++ distributed_sorting/flight_version/gen_map.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     with open("nodelist.txt") as f:
         nodes = f.readline()
-        host_inputfile_map = {n: "~/mcx/distributed_sorting_data/new/4_nodes/records_on_node_"+str(i)+".txt"
+        host_inputfile_map = {n: "~/mcx/distributed_sorting/input_data/4_nodes/records_on_node_"+str(i)+".txt"
                               for i, n in enumerate(nodes.strip("\n").split(","))}
 
     with open("host_inputfile_map.txt", "w") as f:
