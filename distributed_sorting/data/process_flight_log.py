99e3dc85bd159da22fe79bbfc471de3761bafa6f
Chengxin Ma <cxma@protonmail.ch>
2020-03-20 15:07:32 +0100

    Visualize experiment results

--- distributed_sorting/data/process_flight_log.py
+++ distributed_sorting/data/process_flight_log.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import re
 
@@ -10,21 +12,21 @@ def convert_time(time_str):
 class Log:
     hhmmss = r"\d{2}:\d{2}:\d{2}"
 
-    def __init__(self, node_name):
+    def __init__(self, node_name, folder):
         self.node_name = node_name
-        self.times = {"started reading input file": None,
-                      "finished reading input file, started partitioning the records": None,
-                      "finished partitioning the records, started putting them to Plasma": None,
-                      "finished putting partitioned records to Plasma": None,
-                      "send-to-dest started": None,
-                      "putting received data to Plasma finished": None,
-                      "started retrieving the records from Plasma": None,
-                      "finished retrieving the records from Plasma, started converting to DataFrame": None,
-                      "finished converting to DataFrame, started concatenating": None,
-                      "finished concatenating, started sorting the records": None,
-                      "finished sorting the records": None}
-        self.parse_s_log()
-        self.parse_r_log()
+        self.moments = {"started reading input file": None,
+                        "finished reading input file, started partitioning the records": None,
+                        "finished partitioning the records, started putting them to Plasma": None,
+                        "finished putting partitioned records to Plasma": None,
+                        "send-to-dest started": None,
+                        "putting received data to Plasma finished": None,
+                        "started retrieving the records from Plasma": None,
+                        "finished retrieving the records from Plasma, started converting to DataFrame": None,
+                        "finished converting to DataFrame, started concatenating": None,
+                        "finished concatenating, started sorting the records": None,
+                        "finished sorting the records": None}
+        self.parse_s_log(folder)
+        self.parse_r_log(folder)
         self.execution_time = {"partitioning": None,
                                "serialization": None,
                                "communication": None,
@@ -33,82 +35,159 @@ class Log:
                                "sorting": None}
         self.calculate_exec_time()
 
-    def parse_s_log(self):
+    def parse_s_log(self, folder):
         filename = self.node_name+".bullx_s.log"
-        with open(filename) as f:
+        with open(folder+"/"+filename) as f:
             lines = f.readlines()
 
         assert len(lines) == 5
 
-        times = list()
+        moments = list()
         for line in lines:
             match = re.search(Log.hhmmss, line)
-            times.append(match.group())
+            moments.append(match.group())
 
-        self.times["started reading input file"] = times[0]
-        self.times["finished reading input file, started partitioning the records"] = times[1]
-        self.times["finished partitioning the records, started putting them to Plasma"] = times[2]
-        self.times["finished putting partitioned records to Plasma"] = times[3]
-        self.times["send-to-dest started"] = times[4]
+        self.moments["started reading input file"] = moments[0]
+        self.moments["finished reading input file, started partitioning the records"] = moments[1]
+        self.moments["finished partitioning the records, started putting them to Plasma"] = moments[2]
+        self.moments["finished putting partitioned records to Plasma"] = moments[3]
+        self.moments["send-to-dest started"] = moments[4]
 
-    def parse_r_log(self):
+    def parse_r_log(self, folder):
         filename = self.node_name+".bullx_r.log"
-        with open(filename) as f:
+        with open(folder+"/"+filename) as f:
             lines = f.readlines()
 
-        times = list()
+        moments = list()
         for line in lines:
             if "received data, started putting to Plasma" in line:
-                print("Skipped processing starting time of putting to Plasma")
+                # print("Skipped processing starting time of putting to Plasma")
+                pass
             elif "putting received data to Plasma finished" in line:
                 match = re.search(Log.hhmmss, line)
                 t = match.group()
-                print("Updating finishing time of putting received data to Plasma:")
-                print(t)
+                # print("Updating finishing time of putting received data to Plasma:")
+                # print(t)
             else:
                 match = re.search(Log.hhmmss, line)
-                times.append(match.group())
+                moments.append(match.group())
 
-        assert len(times) == 6
+        assert len(moments) == 6
 
-        self.times["putting received data to Plasma finished"] = t
-        self.times["started retrieving the records from Plasma"] = times[0]
-        self.times["finished retrieving the records from Plasma, started converting to DataFrame"] = times[1]
-        self.times["finished converting to DataFrame, started concatenating"] = times[2]
-        self.times["finished concatenating, started sorting the records"] = times[3]
-        self.times["finished sorting the records"] = times[4]
+        self.moments["putting received data to Plasma finished"] = t
+        self.moments["started retrieving the records from Plasma"] = moments[0]
+        self.moments["finished retrieving the records from Plasma, started converting to DataFrame"] = moments[1]
+        self.moments["finished converting to DataFrame, started concatenating"] = moments[2]
+        self.moments["finished concatenating, started sorting the records"] = moments[3]
+        self.moments["finished sorting the records"] = moments[4]
 
     def calculate_exec_time(self):
         self.execution_time["partitioning"] = \
-            convert_time(self.times["finished partitioning the records, started putting them to Plasma"]) - \
-            convert_time(self.times["finished reading input file, started partitioning the records"])
+            convert_time(self.moments["finished partitioning the records, started putting them to Plasma"]) - \
+            convert_time(self.moments["finished reading input file, started partitioning the records"])
         
         self.execution_time["serialization"] = \
-            convert_time(self.times["finished putting partitioned records to Plasma"]) - \
-            convert_time(self.times["finished partitioning the records, started putting them to Plasma"])
+            convert_time(self.moments["finished putting partitioned records to Plasma"]) - \
+            convert_time(self.moments["finished partitioning the records, started putting them to Plasma"])
         
         self.execution_time["communication"] = \
-            convert_time(self.times["putting received data to Plasma finished"]) - \
-            convert_time(self.times["send-to-dest started"])
+            convert_time(self.moments["putting received data to Plasma finished"]) - \
+            convert_time(self.moments["send-to-dest started"])
         
         self.execution_time["deserialization"] = \
-            convert_time(self.times["finished converting to DataFrame, started concatenating"]) - \
-            convert_time(self.times["started retrieving the records from Plasma"])
+            convert_time(self.moments["finished converting to DataFrame, started concatenating"]) - \
+            convert_time(self.moments["started retrieving the records from Plasma"])
         
         self.execution_time["merging"] = \
-            convert_time(self.times["finished concatenating, started sorting the records"]) - \
-            convert_time(self.times["finished converting to DataFrame, started concatenating"])
+            convert_time(self.moments["finished concatenating, started sorting the records"]) - \
+            convert_time(self.moments["finished converting to DataFrame, started concatenating"])
         
         self.execution_time["sorting"] = \
-            convert_time(self.times["finished sorting the records"]) - \
-            convert_time(self.times["finished concatenating, started sorting the records"])
+            convert_time(self.moments["finished sorting the records"]) - \
+            convert_time(self.moments["finished concatenating, started sorting the records"])
 
 
-all_files = os.listdir()
-log_files = [f for f in all_files if ".log" in f]
-node_list = list(set([lf.split(".")[0] for lf in log_files]))
+all_items = os.listdir()
+all_folders = [i for i in all_items if os.path.isdir(i)]
 
-logs = list()
+stats_for_plot = dict()
+    
+for folder in all_folders:
+    stats_for_plot[folder] = dict()
 
-for node in node_list:
-    logs.append(Log(node))
+    all_files = os.listdir(folder)
+    log_files = [f for f in all_files if ".log" in f]
+    node_list = list(set([lf.split(".")[0] for lf in log_files]))
+
+    logs = list()
+
+    for node in node_list:
+        logs.append(Log(node, folder))
+
+    overall_duration = \
+        max([convert_time(log.moments["finished sorting the records"]) for log in logs]) - \
+        min([convert_time(log.moments["finished reading input file, started partitioning the records"])for log in logs])
+
+    stats_for_plot[folder]["overall_duration"] = overall_duration
+
+    overall_stats = {"partitioning": {"mean": None, "std": None},
+                    "serialization": {"mean": None, "std": None},
+                    "communication": {"mean": None, "std": None},
+                    "deserialization": {"mean": None, "std": None},
+                    "merging": {"mean": None, "std": None},
+                    "sorting": {"mean": None, "std": None}}
+
+    for key in overall_stats.keys():
+        overall_stats[key]["mean"] = \
+            np.mean([log.execution_time[key] for log in logs])
+        overall_stats[key]["std"] = \
+            np.std([log.execution_time[key] for log in logs])
+
+    # for log in logs:
+    #     print(log.node_name)
+    #     print(log.execution_time)
+
+    # print(overall_stats)
+
+    stats_for_plot[folder]["overall_stats"] = overall_stats
+
+
+keys = [k for k in stats_for_plot.keys()]
+nums_nodes = sorted(keys, key=lambda s:int(s.split("_")[0]))
+
+overall_durations = [stats_for_plot[num_nodes]["overall_duration"] for num_nodes in nums_nodes]
+par_means = [stats_for_plot[num_nodes]["overall_stats"]["partitioning"]["mean"] for num_nodes in nums_nodes]
+ser_means = [stats_for_plot[num_nodes]["overall_stats"]["serialization"]["mean"] for num_nodes in nums_nodes]
+comm_means = [stats_for_plot[num_nodes]["overall_stats"]["communication"]["mean"] for num_nodes in nums_nodes]
+deser_means = [stats_for_plot[num_nodes]["overall_stats"]["deserialization"]["mean"] for num_nodes in nums_nodes]
+mer_means = [stats_for_plot[num_nodes]["overall_stats"]["merging"]["mean"] for num_nodes in nums_nodes]
+sort_means = [stats_for_plot[num_nodes]["overall_stats"]["sorting"]["mean"] for num_nodes in nums_nodes]
+par_std = [stats_for_plot[num_nodes]["overall_stats"]["partitioning"]["std"] for num_nodes in nums_nodes]
+ser_std = [stats_for_plot[num_nodes]["overall_stats"]["serialization"]["std"] for num_nodes in nums_nodes]
+comm_std = [stats_for_plot[num_nodes]["overall_stats"]["communication"]["std"] for num_nodes in nums_nodes]
+deser_std = [stats_for_plot[num_nodes]["overall_stats"]["deserialization"]["std"] for num_nodes in nums_nodes]
+mer_std = [stats_for_plot[num_nodes]["overall_stats"]["merging"]["std"] for num_nodes in nums_nodes]
+sort_std = [stats_for_plot[num_nodes]["overall_stats"]["sorting"]["std"] for num_nodes in nums_nodes]
+
+x = np.arange(len(nums_nodes))  # the label locations
+width = 0.18  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, overall_durations, width, label="overall duration")
+rects2 = ax.bar(x + width/2, sort_means, width, yerr=sort_std, label="sorting (avg)")
+rects3 = ax.bar(x + width/2, mer_means, width, yerr=mer_std, label="merging (avg)", bottom=sort_means)
+rects4 = ax.bar(x + width/2, deser_means, width, yerr=deser_std, label="deserialization (avg)", bottom=[sum(val) for val in zip(sort_means, mer_means)])
+rects4 = ax.bar(x + width/2, comm_means, width, yerr=comm_std, label="communication (avg)", bottom=[sum(val) for val in zip(sort_means, mer_means, deser_means)])
+rects6 = ax.bar(x + width/2, ser_means, width, yerr=ser_std, label="serialization (avg)", bottom=[sum(val) for val in zip(sort_means, mer_means, deser_means, comm_means)])
+rects7 = ax.bar(x + width/2, par_means, width, yerr=par_std, label="partitioning (avg)", bottom=[sum(val) for val in zip(sort_means, mer_means, deser_means, comm_means, ser_means)])
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel("Time (s)")
+# ax.set_title("")
+ax.set_xticks(x)
+ax.set_xticklabels(nums_nodes)
+ax.legend()
+
+fig.tight_layout()
+plt.savefig("scalability.pdf")
+# plt.show()
