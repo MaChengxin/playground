6faf7de817f66190326f413ef97e7b8d3b20d095
Chengxin Ma <cxma@protonmail.ch>
2020-05-06 17:43:31 +0200

    (Manually) calculated the workload distribution for the case of 4 nodes

--- integ_with_arrow_sam/distribute_workload.py
+++ integ_with_arrow_sam/distribute_workload.py
@@ -7,12 +7,12 @@ WORKLOAD_DISTRIBUTION_LOOKUP = {1: (("chr1", "chr2", "chr3", "chr4", "chr5", "ch
                                 2: (("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8"),
                                     ("chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17",
                                      "chr18", "chr19", "chr20", "chr21", "chr22", "chrM", "chrX", "chrY")),
-                                5: (("chr1", "chr2", "chr3"),
-                                    ("chr4", "chr5", "chr6", "chr7",),
-                                    ("chr8", "chr9", "chr10", "chr11", "chr12"),
-                                    ("chr13", "chr14", "chr15",
-                                     "chr16", "chr17", "chr18"),
-                                    ("chr19", "chr20", "chr21", "chr22", "chrM", "chrX", "chrY"))
+                                4: (("chr1", "chr4", "chr5", "chr8", "chrM"),
+                                    ("chr2", "chr3", "chr6", "chr7"),
+                                    ("chr9", "chr10", "chr11",
+                                     "chr12", "chr13", "chr14"),
+                                    ("chr15", "chr16", "chr17", "chr18", "chr19",
+                                     "chr20", "chr21", "chr22", "chrX", "chrY"))
                                 }
 
 if __name__ == "__main__":
