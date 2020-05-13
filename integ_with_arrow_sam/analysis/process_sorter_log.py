7d04e3fc7f494edcac5e71145606227eef051ce8
Chengxin Ma <cxma@protonmail.ch>
2020-05-13 10:53:42 +0200

    Calculate the duration of each step during sorting

--- integ_with_arrow_sam/analysis/process_sorter_log.py
+++ integ_with_arrow_sam/analysis/process_sorter_log.py
@@ -5,9 +5,11 @@ CHROMOS = ("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9
            "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17",
            "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY", "chrM")
 
+
 def convert_time(time_str):
     h, m, s = time_str.split(":")
     return float(h) * 60 ** 2 + float(m) * 60 + float(s)
+
 
 if __name__ == "__main__":
     re_time = r"\d{2}:\d{2}:\d{2}"
@@ -31,8 +33,24 @@ if __name__ == "__main__":
     per_chromo_sorting_time = {}
     for node in nodes:
         for chromo_id in per_chromo_timestamps[node]:
-            per_chromo_sorting_time[chromo_id] = per_chromo_timestamps[node][chromo_id][-1] - per_chromo_timestamps[node][chromo_id][0]
+            per_chromo_sorting_time[chromo_id] = {}
+            per_chromo_sorting_time[chromo_id]["retrieve_from_plasma"] = per_chromo_timestamps[node][chromo_id][1] - \
+                per_chromo_timestamps[node][chromo_id][0]
+            per_chromo_sorting_time[chromo_id]["convert_RB_to_DF"] = per_chromo_timestamps[node][chromo_id][2] - \
+                per_chromo_timestamps[node][chromo_id][1]
+            per_chromo_sorting_time[chromo_id]["merge"] = per_chromo_timestamps[node][chromo_id][3] - \
+                per_chromo_timestamps[node][chromo_id][2]
+            per_chromo_sorting_time[chromo_id]["sort"] = per_chromo_timestamps[node][chromo_id][4] - \
+                per_chromo_timestamps[node][chromo_id][3]
+            per_chromo_sorting_time[chromo_id]["put_back_to_plasma"] = per_chromo_timestamps[node][chromo_id][5] - \
+                per_chromo_timestamps[node][chromo_id][4]
 
     with open("sorting_time_N_nodes.txt", "w") as f:
+        f.write("\tretrieve_from_plasma\tconvert_RB_to_DF\tmerge\tsort\tput_back_to_plasma\n")
         for chromo in CHROMOS:
-            f.write(chromo + "\t" + str(per_chromo_sorting_time[chromo]) + "\n")
+            f.write(chromo + "\t" +
+                    str(per_chromo_sorting_time[chromo]["retrieve_from_plasma"]) + "\t" +
+                    str(per_chromo_sorting_time[chromo]["convert_RB_to_DF"]) + "\t" +
+                    str(per_chromo_sorting_time[chromo]["merge"]) + "\t" +
+                    str(per_chromo_sorting_time[chromo]["sort"]) + "\t" +
+                    str(per_chromo_sorting_time[chromo]["put_back_to_plasma"]) + "\n")
